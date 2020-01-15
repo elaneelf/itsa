d45cd6f9f14dc714368a5db5e87206f02f7e0a76
elaneelf <57987305+elaneelf@users.noreply.github.com>
2020-01-15 20:42:50 +0800

    判斷座標是否在圓形的範圍內

---
+++ m16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x, y, d;
+    while(scanf("%d %d", &x, &y)!=EOF)
+    {
+        d = x*x + y*y;
+        if(d<=10000)
+            printf("inside\n");
+        else
+            printf("outside\n");
+    }
+
+
+    return 0;
+}
+
+

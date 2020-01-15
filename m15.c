d12cd8268d22d31e8cee513ca086154085ca1db8
elaneelf <57987305+elaneelf@users.noreply.github.com>
2020-01-15 20:42:25 +0800

    判斷座標是否在正方形的範圍內

---
+++ m15.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x, y, t;
+    while(scanf("%d %d", &x, &y)!=EOF)
+    {
+        if(x<=100 && y<=100)
+            printf("inside\n");
+        else
+            printf("outside\n");
+    }
+
+
+    return 0;
+}
+
